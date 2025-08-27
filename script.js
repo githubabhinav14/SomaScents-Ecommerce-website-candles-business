8864aa1eb9eff8ecacbe12d908e526ec5485cc43
githubabhinav14 <githubabhinav14@gmail.com>
2025-08-27 12:36:00 +0530

    changed email and phone number

--- script.js
+++ script.js
@@ -1296,11 +1296,11 @@ function renderHomePageAndAllSections() {
                     <h3>Our Details</h3>
                     <div class="contact-item">
                         <svg class="contact-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-                        <p><a href="mailto:somascents@gmail.com">somascents@gmail.com</a></p>
+                        <p><a href="mailto:somascents5@gmail.com">somascents5@gmail.com</a></p>
                     </div>
                     <div class="contact-item">
                         <svg class="contact-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.774a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74A1 1 0 0118 16.847V17a1 1 0 01-1 1H3a1 1 0 01-1-1V3z"></path></svg>
-                        <p>+91 8919847675</p>
+                        <p>+91 7416778158</p>
                     </div>
                     <div class="contact-item">
                         <svg class="contact-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path></svg>
